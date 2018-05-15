24cf4b94c5fbae8f92b138607d0b4f984bab21ac
sfali <sali@loyalty.com>
2018-05-15 08:43:29 -0400

    updated implementation for SqsNotification

--- src/main/scala/com/loyalty/testing/s3/notification/actor/NotificationActor.scala
+++ src/main/scala/com/loyalty/testing/s3/notification/actor/NotificationActor.scala
@@ -1,14 +1,17 @@
 package com.loyalty.testing.s3.notification.actor
 
 import akka.actor.{Actor, ActorLogging, Props}
+import com.amazonaws.services.sns.AmazonSNSAsync
+import com.amazonaws.services.sqs.AmazonSQSAsync
+import com.loyalty.testing.s3.Settings
 import com.loyalty.testing.s3.notification.NotificationData
-import com.loyalty.testing.s3.{Settings, SnsSettings, SqsSettings}
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
 
-class NotificationActor(sqsSettings: SqsSettings, snsSettings: SnsSettings) extends Actor with ActorLogging {
+class NotificationActor(sqsClient: AmazonSQSAsync, snsClient: AmazonSNSAsync) extends Actor with ActorLogging {
 
+  import context.dispatcher
   import NotificationActor._
   import com.loyalty.testing.s3.notification._
 
@@ -18,17 +21,23 @@ class NotificationActor(sqsSettings: SqsSettings, snsSettings: SnsSettings) exte
   }
 
   override def receive: Receive = {
-    case SqsNotification(name, notificationData) =>
-      sqsSettings.maybeSqsClient match {
-        case Some(amazonSQSAsync) =>
-          Future.successful(amazonSQSAsync.sendMessageAsync(
-            sqsSettings.maybeQueueUrl.get,
-            generateSqsMessage(name, notificationData)).get())
+    case SqsNotification(queueUrl, configName, notificationData) =>
+      val message = generateSqsMessage(configName, notificationData)
+      log.info("Sending notification: {}", message)
 
-        case None => log.info("No Sqs client is setup, notification is disabled.")
-      }
+      Future
+        .successful(
+          sqsClient
+            .sendMessageAsync(queueUrl, message)
+            .get())
+        .recover {
+          case ex => log.warning(
+            """
+              |Unable to send message to queue {}, error message:- {}:{}
+            """.stripMargin, queueUrl, ex.getClass.getName, ex.getMessage)
+        }
 
-    case SnsNotification(name, notificationData) => throw new RuntimeException("unimplemented")
+    case SnsNotification(topicArn, configName, notificationData) => throw new RuntimeException("unimplemented")
 
     case msg => log.warning("Unhandled message: {}", msg)
   }
@@ -37,9 +46,11 @@ class NotificationActor(sqsSettings: SqsSettings, snsSettings: SnsSettings) exte
 
 object NotificationActor {
 
-  def props()(implicit settings: Settings): Props = Props(new NotificationActor(settings.sqs, settings.sns))
+  def props()(implicit settings: Settings): Props =
+    Props(new NotificationActor(settings.sqs.sqsClient, settings.sns.snsClient))
 
-  case class SqsNotification(name: String, notificationData: NotificationData)
+  case class SqsNotification(queueUrl: String, configName: String, notificationData: NotificationData)
 
-  case class SnsNotification(name: String, notificationData: NotificationData)
+  case class SnsNotification(topicArn: String, configName: String, notificationData: NotificationData)
+
 }
