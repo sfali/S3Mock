d378b11bba4ede23baf9b048c183e7cdab72e88b
sali <sali@loyalty.com>
2019-12-17 15:14:55 -0500

    initialize guardian actor

--- src/main/scala/com/loyalty/testing/s3/MainTyped.scala
+++ src/main/scala/com/loyalty/testing/s3/MainTyped.scala
@@ -2,34 +2,47 @@ package com.loyalty.testing.s3
 
 import java.nio.file.Paths
 
-import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.adapter._
+import akka.actor.typed.{ActorSystem, Behavior}
 import akka.actor.{ActorSystem => ClassicActorSystem}
-import akka.util.Timeout
-import com.loyalty.testing.s3.actor.SpawnBehavior
 import com.loyalty.testing.s3.repositories.{NitriteDatabase, ObjectIO}
 import com.loyalty.testing.s3.service.NotificationService
 import com.loyalty.testing.s3.streams.FileStream
 import com.loyalty.testing.s3.utils.DateTimeProvider
 import com.typesafe.config.ConfigFactory
 
-import scala.concurrent.duration._
-
 object MainTyped extends App {
 
-  private val config = ConfigFactory.load()
-  private implicit val system: ActorSystem[SpawnBehavior.Command] = ActorSystem(SpawnBehavior(),
-    config.getString("app.name"))
-  private val classicSystem: ClassicActorSystem = system.toClassic
-  private implicit val dateTimeProvider: DateTimeProvider = DateTimeProvider()
-  private implicit val settings: AppSettings = AppSettings(system.settings.config)
-  implicit val timeout: Timeout = Timeout(3.seconds)
+  private val config = ConfigFactory.load("application-clustered")
 
-  private val root = Paths.get(System.getProperty("user.dir"), ".s3mock")
-  system.log.info("Root path of S3: {}", root.toAbsolutePath)
+  private sealed trait GuardianRequest
 
-  private val objectIO = ObjectIO(root, FileStream())
-  private val database = NitriteDatabase(root)
-  private val notificationService = NotificationService(settings.awsSettings)(classicSystem)
+  private final case object InitializeApp extends GuardianRequest
+
+  private val guardian: Behavior[Nothing] =
+    Behaviors.setup[GuardianRequest] {
+      ctx =>
+        implicit val system: ActorSystem[Nothing] = ctx.system
+        val classicSystem: ClassicActorSystem = system.toClassic
+        implicit val settings: AppSettings = AppSettings(system.settings.config)
+        implicit val dateTimeProvider: DateTimeProvider = DateTimeProvider()
+
+        ctx.self ! InitializeApp
+
+        Behaviors.receiveMessage {
+          case InitializeApp =>
+            val root = Paths.get(System.getProperty("user.dir"), ".s3mock")
+            system.log.info("Root path of S3: {}", root.toAbsolutePath)
+
+            val objectIO = ObjectIO(root, FileStream())
+            val database = NitriteDatabase(root)
+            val notificationService = NotificationService(settings.awsSettings)(classicSystem)
+
+            Behaviors.same
+        }
+    }.narrow
+
+  ActorSystem[Nothing](guardian, config.getString("app.name"), config)
 
 }
